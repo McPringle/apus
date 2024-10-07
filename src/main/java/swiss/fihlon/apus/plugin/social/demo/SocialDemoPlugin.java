8eeaf8d849ac0d7c34c8ed7b86155fa88c9e82ac
Marcus Fihlon <marcus@fihlon.swiss>
2024-10-07 08:52:50 +0200

    🚨 Fix linter warnings #104 #225
    Signed-off-by: Marcus Fihlon <marcus@fihlon.swiss>

--- src/main/java/swiss/fihlon/apus/plugin/social/demo/SocialDemoPlugin.java
+++ src/main/java/swiss/fihlon/apus/plugin/social/demo/SocialDemoPlugin.java
@@ -1,3 +1,20 @@
+/*
+ * Apus - A social wall for conferences with additional features.
+ * Copyright (C) Marcus Fihlon and the individual contributors to Apus.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
 package swiss.fihlon.apus.plugin.social.demo;
 
 import net.datafaker.Faker;
@@ -15,7 +32,7 @@ import java.util.UUID;
 import java.util.stream.Stream;
 
 @Service
-public class SocialDemoPlugin implements SocialPlugin {
+public final class SocialDemoPlugin implements SocialPlugin {
 
     private static final Locale LOCALE = Locale.getDefault();
     private static final Random RANDOM = new Random();
@@ -29,39 +46,39 @@ public class SocialDemoPlugin implements SocialPlugin {
     public Stream<Post> getPosts() {
         final Faker faker = new Faker(LOCALE, RANDOM);
         final var posts = new ArrayList<Post>();
-        for(int i = 0;i<30;i++){
+        for (int i = 0; i < 30; i++) {
             posts.add(
                     new Post(generateId(),
                             getRandomDateTime(),
-                            getRandomAutor(faker),
+                            getRandomAuthor(faker),
                             getRandomAvatar(faker),
                             getRandomProfile(faker),
                             getRandomHtml(faker),
                             getRandomImage(faker),
                             false,
-                            false )
+                            false)
             );
         }
         return posts.stream();
     }
 
-    private @NotNull List<String> getRandomImage(Faker faker) {
+    private @NotNull List<String> getRandomImage(@NotNull final Faker faker) {
         return List.of(faker.image().base64SVG());
     }
 
-    private @NotNull String getRandomHtml(Faker faker) {
+    private @NotNull String getRandomHtml(@NotNull final Faker faker) {
         return faker.lorem().sentence(10);
     }
 
-    private @NotNull String getRandomProfile(Faker faker) {
+    private @NotNull String getRandomProfile(@NotNull final Faker faker) {
         return faker.internet().emailAddress();
     }
 
-    private @NotNull String getRandomAvatar(Faker faker) {
+    private @NotNull String getRandomAvatar(@NotNull final Faker faker) {
         return faker.avatar().image();
     }
 
-    private static @NotNull String getRandomAutor(Faker faker) {
+    private static @NotNull String getRandomAuthor(@NotNull final Faker faker) {
         return faker.name().fullName();
     }
 
