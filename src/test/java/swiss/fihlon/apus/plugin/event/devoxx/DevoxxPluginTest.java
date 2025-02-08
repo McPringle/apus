c0c3cff0924f63af7dddd08400728ee6fe96e6a6
Marcus Fihlon <marcus@fihlon.swiss>
2025-02-08 18:37:46 +0100

    ✅ Add tests to read devoxx sessions
    Signed-off-by: Marcus Fihlon <marcus@fihlon.swiss>

--- src/test/java/swiss/fihlon/apus/plugin/event/devoxx/DevoxxPluginTest.java
+++ src/test/java/swiss/fihlon/apus/plugin/event/devoxx/DevoxxPluginTest.java
@@ -23,10 +23,18 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 import swiss.fihlon.apus.configuration.AppConfig;
+import swiss.fihlon.apus.event.Language;
+import swiss.fihlon.apus.event.Room;
+import swiss.fihlon.apus.event.Session;
+import swiss.fihlon.apus.event.SessionImportException;
+import swiss.fihlon.apus.event.Speaker;
 
+import java.time.LocalDateTime;
 import java.util.stream.Stream;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
@@ -75,6 +83,51 @@ class DevoxxPluginTest {
 
         final var devoxxPlugin = new DevoxxPlugin(appConfig);
         assertTrue(devoxxPlugin.isEnabled());
+    }
+
+    @Test
+    void getSessions() {
+        final var appConfig = mock(AppConfig.class);
+        final var devoxxConfig = new DevoxxConfig(
+                "file:src/test/resources/devoxx.json?eventId=%s&weekday=%s",
+                "BBAD", "monday");
+        when(appConfig.devoxx()).thenReturn(devoxxConfig);
+
+        final var devoxxPlugin = new DevoxxPlugin(appConfig);
+        final var sessions = devoxxPlugin.getSessions().toList();
+        assertEquals(8, sessions.size());
+
+        final var sessionIds = sessions.stream().map(Session::id).toList();
+        for (int counter = 1; counter <= 8; counter++) {
+            final var sessionId = String.format("BBAD:%d", counter);
+            assertTrue(sessionIds.contains(sessionId));
+        }
+
+        // full check of session with ID "BBAD:5"
+        final var session = sessions.get(4);
+        assertEquals("BBAD:5", session.id());
+        assertEquals(LocalDateTime.of(2024, 1, 3, 19, 0), session.startDate());
+        assertEquals(LocalDateTime.of(2024, 1, 3, 19, 45), session.endDate());
+        assertEquals(new Room("Room A"), session.room());
+        assertEquals("Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat", session.title());
+        assertEquals(2, session.speakers().size());
+        assertEquals(new Speaker("Saul Goodman", "https://foo.bar/03415469-efed-4fde-b98c-b0e2db6225e7.png"),
+                session.speakers().get(0));
+        assertEquals(new Speaker("Mike Ehrmantraut", "https://foo.bar/0ad4f2e3-36c6-4093-b492-8098fa7e5560.png"),
+                session.speakers().get(1));
+        assertEquals(Language.UNKNOWN, session.language());
+    }
+
+    @Test
+    void parseExceptionSessions() {
+        final var appConfig = mock(AppConfig.class);
+        final var devoxxConfig = new DevoxxConfig(
+                "file:src/test/resources/devoxx-broken.json?eventId=%s&weekday=%s",
+                "1", "monday");
+        when(appConfig.devoxx()).thenReturn(devoxxConfig);
+
+        final var devoxxPlugin = new DevoxxPlugin(appConfig);
+        assertThrows(SessionImportException.class, devoxxPlugin::getSessions);
     }
 
 }
