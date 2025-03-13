02c19bd30e9ed937f2006916ea71fa4d83067b03
Marcus Fihlon <marcus@fihlon.swiss>
2025-03-13 11:56:00 +0100

    ✅ Add support to run testcontainer on a fixed port #293
    Signed-off-by: Marcus Fihlon <marcus@fihlon.swiss>

---
+++ src/test/java/swiss/fihlon/apus/util/FixedPortContainer.java
@@ -0,0 +1,30 @@
+/*
+ * Apus - A social wall for conferences with additional features.
+ * Copyright (C) Marcus Fihlon and the individual contributors to Apus.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+package swiss.fihlon.apus.util;
+
+import org.jetbrains.annotations.NotNull;
+import org.testcontainers.containers.GenericContainer;
+
+public class FixedPortContainer extends GenericContainer<FixedPortContainer> {
+
+    public FixedPortContainer(@NotNull final String dockerImageName, final int fixedPort, final int containerPort) {
+        super(dockerImageName);
+        addFixedExposedPort(fixedPort, containerPort);
+    }
+
+}
